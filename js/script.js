06a2d419c266a62611f7da1771f374fc50931f7f
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-06 15:21:38 +0200

    aggiunto alert numeri random

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+var numeriRandom=[];
+
+for(var i=0; i<5;i++){
+    var numeriGenerati=parseInt(Math.floor(Math.random()*30)+1);
+    numeriRandom.push(numeriGenerati);      
+}
+alert(numeriRandom);
+console.log(numeriRandom); 
+//timer//
+var time=30;
+
+setTimeout(function(){ 
+    document.getElementById("conteggio").innerHTML
+}, 1000);
