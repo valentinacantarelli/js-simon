53908d98b7ebe788745c7473dcf92c76615fdb77
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-06 17:46:48 +0200

    impostata funzione timing

--- js/script.js
+++ js/script.js
@@ -4,23 +4,43 @@
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-var numeriRandom=[];
+//funzione di verifica
+function inArray(arr, el) {
+	var count = 0;
+	while ( count < arr.length ) {
+		if ( arr[count] == el ) {
+			return true;
+		}
+		count++;
+	}
+	return false;
+}
 
+var numeriRandom=[];
+var numeriInseriti=[];
+
+//genero numeri random
 for(var i=0; i<5;i++){
     var numeriGenerati=parseInt(Math.floor(Math.random()*30)+1);
     numeriRandom.push(numeriGenerati);      
 }
 alert(numeriRandom);
-console.log(numeriRandom); 
+
 //timer//
-var time=30;
+var time=10;
 var conteggio=setInterval(
     function(){ 
         document.getElementById("conteggio").innerHTML=time;
         time--;
         if(time==-1){
             clearInterval(conteggio);
-            
+            for(var i=0;i<5;i++){                
+                var numeriUtente=parseInt(prompt("inserisci i numeri che ricordi!"));
+
+                if(inArrey(numeriInseriti[i])==true && inArrey(numeriRandom[i])==true){
+                    alert("Hai indovinato tutti i numeri!");{}
+                }
+            }
         }
     
     }, 1000
